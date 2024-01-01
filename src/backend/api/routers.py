c0dac97c64d8a94b12a210d550ff96d2923e01cb
Michael Nguyen <michaelnguyen1195@gmail.com>
2024-01-01 22:59:50 +0700

    Handling FastAPI WebSocket disconnections

--- src/backend/api/routers.py
+++ src/backend/api/routers.py
@@ -5,18 +5,31 @@ from schemas.message import ChatResponse
 
 router = APIRouter()
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+manager = ConnectionManager()
+
 @router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
+    await manager.connect(websocket)
     if not settings.OPENAI_API_KEY.startswith("sk-"):
             await websocket.send_json({"error": "OPENAI_API_KEY is not set"})
             return
 
     stream_hanlder = StreamingLLMCallbackHandler(websocket)
     conversation_chain = get_openai_chain(stream_hanlder)
-    while True:
-        try:
-            # Receive and send back the client message
+    try:
+        while True:
+             # Receive and send back the client message
             user_msg = await websocket.receive_text()
             resp = ChatResponse(sender="human", message=user_msg, type="stream")
             await websocket.send_json(resp.dict())
@@ -35,13 +48,12 @@ async def websocket_endpoint(websocket: WebSocket):
             # Send the end-response back to the client
             end_resp = ChatResponse(sender="bot", message="", type="end")
             await websocket.send_json(end_resp.dict())
-        except WebSocketDisconnect:
-            await websocket.close()
-            break
-        except Exception as e:
-            resp = ChatResponse(
-                sender="bot",
-                message="Sorry, something went wrong. Try again.",
-                type="error",
-            )
-            await websocket.send_json(resp.dict())
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+    except Exception as e:
+        resp = ChatResponse(
+            sender="bot",
+            message="Sorry, something went wrong. Try again.",
+            type="error",
+        )
+        await websocket.send_json(resp.dict())
